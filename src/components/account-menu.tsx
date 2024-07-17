5641dba6f023f2e2f9b8e36ae4922d49f79d0d99
gabenasci <gabenascimento1@gmail.com>
2024-07-17 17:38:48 -0300

    add login and signup pages

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Forno Italicus
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Gabriel Nascimento</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            gabriel@fornoitalicus.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Restaurant profile</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sign Out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router: ReturnType<typeof createBrowserRouter> =
   createBrowserRouter([
     {
       path: '/',
-      element: <Dashboard />,
+      element: <AppLayout />,
+      children: [{ path: '/', element: <Dashboard /> }],
     },
     {
-      path: '/sign-in',
-      element: <SignIn />,
+      path: '/',
+      element: <AuthLayout />,
+      children: [
+        { path: '/sign-in', element: <SignIn /> },
+        { path: '/sign-up', element: <SignUp /> },
+      ],
     },
   ])
