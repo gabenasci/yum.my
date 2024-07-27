f52cb018009c969d479cf63bfa40a75abc6f895a
gabenasci <gabenascimento1@gmail.com>
2024-07-26 22:40:41 -0300

    add react helmet title to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,20 @@
+import { Helmet } from 'react-helmet-async'
 import { Link } from 'react-router-dom'
 
 export function NotFound() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-4xl font-bold">Page not found</h1>
-      <p className="text-accent-foreground">
-        Return to{' '}
-        <Link to="/" className="text-sky-500 dark:text-sky-400">
-          Dashboard
-        </Link>
-      </p>
-    </div>
+    <>
+      <Helmet title="404">
+        <div className="flex h-screen flex-col items-center justify-center gap-2">
+          <h1 className="text-4xl font-bold">Page not found</h1>
+          <p className="text-accent-foreground">
+            Return to{' '}
+            <Link to="/" className="text-sky-500 dark:text-sky-400">
+              Dashboard
+            </Link>
+          </p>
+        </div>
+      </Helmet>
+    </>
   )
 }
